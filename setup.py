76171e8847a0f07ecce83eba51dbb9bac311e4b7
Your Name <you@example.com>
2021-04-21 14:14:44 -0500

    adding package dependencies

--- setup.py
+++ setup.py
@@ -1,10 +1,14 @@
 from distutils.core import setup
 from  setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
     name="Myportfolio",
     version="1.0",
     packages=find_packages(),
+    install_requires=required,
     entry_points = {
         "console_scripts": [
             "myportfolio=myportfolio.entrypoint:main"
